c0f3e8d930691a3b30fcf826c2be8fd84ff5c536
Logan McGuire <lrmcguire93@gmail.com>
2020-04-21 18:52:49 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   #array.index(value_to_find);
-  matching_index;
+  matching_index = [];
   if array.include?(value_to_find)
     array.length.times { |index|
       if array[index] == value_to_find 
